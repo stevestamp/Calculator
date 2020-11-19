484945385d4b99d365d10539351165c2688f7382
Stephen Stamp <71839160+stevestamp@users.noreply.github.com>
2020-11-19 09:57:44 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Stephen Stamp on 11/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
